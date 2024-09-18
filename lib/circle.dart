2d6d0e82ad58a2e1feaa71a520835bfdf86ad420
inshad02 <inshadkorambayil@gmail.com>
2024-09-18 09:49:16 +0530

    17/09/2024

---
+++ lib/circle.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Circle extends StatefulWidget {
+  const Circle({super.key});
+
+  @override
+  State<Circle> createState() => _CircleState();
+}
+
+class _CircleState extends State<Circle> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              border: Border.all(), borderRadius: BorderRadius.circular(50)),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chess_board/chess_board.dart';
+import 'package:demo/Circle.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChessBoard(),
+      home: Circle(),
     );
   }
 }
